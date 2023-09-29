da49593b5d8a41ef5027f57c4a37ab28054c1c12
Emil-Spi <xiriusix@gmail.com>
2023-09-29 14:35:35 -0400

    weather app step 4

--- src/components/BodyCard.jsx
+++ src/components/BodyCard.jsx
@@ -6,11 +6,12 @@ const { main:{ temp, temp_min, temp_max, humidity }, name } = weaterinfo;
 
   return (
     <div className="card-body">
-        <h5 className="card-title">{ name }</h5>
+        <h5 className="card-title fs-2 text-primary">{ name }</h5>
+          <hr  className="text-primary " />
             <p className="card-text">Temperature: <strong>{ parseInt(temp - 273.15) } °C</strong></p>
             <p className="card-text">Temperature min: <strong>{ parseInt(temp_min - 273.15) } °C</strong></p>
             <p className="card-text">Temperature max: <strong>{ parseInt(temp_max - 273.15) } °C</strong></p>
-            <p className="card-text">Humidity: <strong>{ humidity } </strong></p>                                        
+            <p className="card-text">Humidity: <strong>{ humidity } %</strong></p>                                        
         </div>
   )
 }
