d49f0a99c80ba9667c06910c30575ff5dcfb471b
Emil-Spi <xiriusix@gmail.com>
2025-09-29 12:47:09 -0400

    v1.3

--- src/pages/MentorTimer.jsx
+++ src/pages/MentorTimer.jsx
@@ -5,26 +5,31 @@ export const MentorTimer = () => {
 
 
   const [time, setTime] = useState(new Date())
-  const [endTime, setEndTime] = useState('08-14-2026')
+  const [endTime, setEndTime] = useState('')
+  const [resume, setResume] = useState('')
+
+  const [tempEndTime, setTempEndTime] = useState('04-04-2026')
+  const [tempResume, setTempResume] = useState('')
+
 
 
   useEffect(() => {
     const interval = setInterval(() => {
-      setTime( new Date)
+      setTime(new Date)
     }, 1000);
-  
+
     return () => clearInterval(interval)
   }, [])
-        
 
-const getTimerResult = (time, endtime) => {
+
+  const getTimerResult = (time, endtime) => {
 
     time = new Date()
     const remainTime = (new Date(endtime) - time + 1000) / 1000;
-    const remainDays =  Math.floor( remainTime / (3600 * 24)  )
-    const remainHours =  ('0' +  Math.floor( remainTime / 3600 % 24  )).slice(-2)
-    const remainMinutes =  ('0' +  Math.floor( remainTime / 60 % 60  )).slice(-2)
-    const remainSeconds =  ('0' +  Math.floor( remainTime % 60 )).slice(-2)
+    const remainDays = Math.floor(remainTime / (3600 * 24))
+    const remainHours = ('0' + Math.floor(remainTime / 3600 % 24)).slice(-2)
+    const remainMinutes = ('0' + Math.floor(remainTime / 60 % 60)).slice(-2)
+    const remainSeconds = ('0' + Math.floor(remainTime % 60)).slice(-2)
 
 
     return {
@@ -33,46 +38,74 @@ const getTimerResult = (time, endtime) => {
       remainMinutes,
       remainSeconds
     }
-}
+  }
 
- const { remainDays,remainHours,remainSeconds,remainMinutes  } = getTimerResult(time, endTime)// put your date here
+  const { remainDays, remainHours, remainSeconds, remainMinutes } = getTimerResult(time, tempEndTime)// putyour date here
 
   // if (remainDays < 1 && remainHours < 1 && remainMinutes < 1 && remainSeconds < 1 ) {
-    // condition here
+  // condition here
   // }
+
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    setResume(tempResume)
+    setTempEndTime(endTime)
+    setTempResume('')
+  }
 
   return (
     <>
-    <div className="container1 row">
-    <SideBar />
- 
-      <div className='wave'></div>
-     <div className="container col-9 d-flex align-items-center justify-content-center flex-column animate__animated  animate__fadeIn">
+      <div className="container1 row">
+        <SideBar />
 
 
-       <h1 className="title2">Mentor timer</h1>
+        <div className='wave'></div>
+        <div className="container col-9 d-flex align-items-center justify-content-center flex-column animate__animated  animate__fadeIn">
 
-       <h1 className="title__coming">Coming soon </h1>
-          <div  className='timer d-flex'>
-            <p className={ `timerNumber ${remainDays < 1 ?"animate__animated animate__bounce text-light" : "" } `} > {remainDays}  <strong>Days</strong> </p>
-            <p className={ `timerNumber ${remainHours < 1  ?"animate__animated animate__bounce text-light" : "" } `} > {remainHours} <strong>Hours</strong></p>
-            <p className={ `timerNumber ${remainMinutes < 1 ?"animate__animated animate__bounce text-light" : "" } `}> {remainMinutes} <strong>Minutes</strong> </p>
-            <p className={ `timerNumber ${remainSeconds === "00" ?"animate__animated animate__bounce text-light" : "" } `}> {remainSeconds} <strong>seconds</strong></p>
+
+          <h1 className="title2">Mentor timer</h1>
+
+          <h1 className="title__coming">Coming soon </h1>
+          {resume && endTime && <h1 className='title2'>{`${resume} - ${endTime}`}</h1>}
+
+          <div className='timer d-flex'>
+            <p className={`timerNumber ${remainDays < 1 ? "animate__animated animate__bounce text-light" : ""} `} > {remainDays}  <strong>Days</strong> </p>
+            <p className={`timerNumber ${remainHours < 1 ? "animate__animated animate__bounce text-light" : ""} `} > {remainHours} <strong>Hours</strong></p>
+            <p className={`timerNumber ${remainMinutes < 1 ? "animate__animated animate__bounce text-light" : ""} `}> {remainMinutes} <strong>Minutes</strong> </p>
+            <p className={`timerNumber ${remainSeconds === "00" ? "animate__animated animate__bounce text-light" : ""} `}> {remainSeconds} <strong>seconds</strong></p>
           </div>
 
-          <h2 className='selectTime'>Put a date</h2>
-          <input 
-          type="date" 
-          className='putDate'
-          value={endTime}
-          onChange={ e => setEndTime(e.target.value) }
-          />
+
+          <h2 className='selectTime'>Summary</h2>
+
+          <form className='formSelect' onSubmit={onSubmit}>
+
+            <input
+              type="text"
+              className='putDate'
+              value={tempResume}
+              onChange={e => setTempResume(e.target.value)}
+            />
+
+            <input
+              type="date"
+              className='putDate'
+              value={tempEndTime}
+              onChange={e => setEndTime(e.target.value)}
+            />
+
+            <button type='submit' className='boton'>
+              Confirm
+            </button>
+
+          </form>
 
 
-      </div>   
-     </div>
- 
-     </>
+        </div>
+      </div>
+
+    </>
   )
 }
 
